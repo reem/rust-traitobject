99b1993a13bf80e93031048586526384d1d8bddc
Steven Fackler <sfackler@gmail.com>
2017-05-23 22:14:32 -0700

    Simplify data functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,24 @@
 //!
 //! Unsafe helpers for working with raw TraitObjects.
 
-use std::mem;
-
 /// Get the data pointer from this trait object.
 ///
 /// Highly unsafe, as there is no information about the type of the data.
 pub unsafe fn data<T: ?Sized>(val: *const T) -> *const () {
-    *mem::transmute::<*const *const T, *const *const ()>(&val)
+    val as *const ()
 }
 
 /// Get the data pointer from this trait object, mutably.
 ///
 /// Highly unsafe, as there is no information about the type of the data.
-pub unsafe fn data_mut<T: ?Sized>(mut val: *mut T) -> *mut () {
-    *mem::transmute::<*mut *mut T, *mut *mut ()>(&mut val)
+pub unsafe fn data_mut<T: ?Sized>(val: *mut T) -> *mut () {
+    val as *mut ()
 }
 
 #[test]
 fn test_simple() {
     let x = &7 as &Send;
-    unsafe { assert!(&7 == mem::transmute::<_, &i32>(data(x))) };
+    unsafe { assert!(&7 == std::mem::transmute::<_, &i32>(data(x))) };
 }
 
 /// A trait implemented for all trait objects.
